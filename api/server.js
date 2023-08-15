e7e43ea0a83d526e4825504399ebd5b67274cf75
Tyler Oakley <tyler.oakley@briebug.com>
2023-08-15 11:49:54 -0600

    Add id field

--- api/server.js
+++ api/server.js
@@ -104,11 +104,13 @@ const router = jsonServer.router({
 const router = jsonServer.router({
   "students": [
        {
+        "id" : "1",
       "name": "John Johnson",
       "year": "Sophomore",
       "grade": "B"
     },
     {
+      "id" : "2",
       "name": "Jack Jackson",
       "year": "Senior",
       "grade": "F"
